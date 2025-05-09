93ce034388b71523862e9a88d55f0e44611f62c8
NHI <khainhi03@gmail.com>
2025-05-09 13:06:24 +0700

    add cart model

---
+++ models/cart.js
@@ -0,0 +1,20 @@
+module.exports = function Cart(oldCart) {
+    this.items = oldCart.items || {};
+    this.totalQty = oldCart.totalQty || 0;
+    this.totalPrice = oldCart.totalPrice || 0;
+    this.numItems = oldCart.numItems || 0;
+  
+    this.add = (item, id, qty) => {
+      const itemQty = qty ? Number(qty) : 1;
+      var storeItem = this.items[id];
+      if (!storeItem) {
+        storeItem = this.items[id] = { item: item, qty: 0, price: 0, images: '' };
+        this.numItems++;
+      }
+      storeItem.qty += itemQty;
+      storeItem.price = storeItem.item.price * storeItem.qty;
+      storeItem.images = storeItem.item.images[0];
+      this.totalQty += itemQty;
+      this.totalPrice += storeItem.item.price;
+    };
+}
