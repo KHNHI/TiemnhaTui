8fb4dd71513a49bf1110abd5464a10ffa45763df
THAO VY <lengocthaovy1587@gmail.com>
2025-05-12 15:58:24 +0700

    Comments

---
+++ models/productCategory.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const productCategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  // tương tự code, là trường con (aothun, aohoodie, ...)
+  // tương tự name , là trường con mà viết hoa có dấu ...
+  childName: {
+    type: [String],
+    required: true
+  }
+});
+// thể loại sản phẩm
+//xử lý commit
+const productCategory = mongoose.model(
+  "productCategory",
+  productCategorySchema
+);
+module.exports = productCategory;
