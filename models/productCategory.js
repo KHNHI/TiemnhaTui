54c9824ca315cb645c111807f3696822d7a89d83
THAO VY <lengocthaovy2435@gmail.com>
2025-05-12 16:05:24 +0700

    xulycommit

--- models/productCategory.js
+++ models/productCategory.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+// const productCategory = require("./productCategory");
+// const productCategory = require("./productCategory");
 
 const productCategorySchema = new Schema({
   name: {
